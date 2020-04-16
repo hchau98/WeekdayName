4b7df2006f6f5419c5e6d8740ea72479c63a99ce
hchau98 <chauhenry02@gmail.com>
2020-04-16 18:25:52 -0400

    Second Commit

--- src/WeekdayName.java
+++ src/WeekdayName.java
@@ -6,22 +6,28 @@ public class WeekdayName {
         while(num>=1 || num<=7) {
             System.out.println("Enter in a number between 1-7:");
             num = input.nextInt();
-        }
-        if(num==1){
-            System.out.println("It is Sunday");
-        }
-        if(num==2){
-            System.out.println("It is Monday");
-        }if(num==3){
-            System.out.println("It is Tuesday");
-        }if(num==4){
-            System.out.println("It is Wednesday");
-        }if(num==5){
-            System.out.println("It is Thrusday");
-        }if(num==6){
-            System.out.println("It is Friday");
-        }if(num==7) {
-            System.out.println("It is Saturday");
+
+            if (num == 1) {
+                System.out.println("It is Sunday");
+            }
+            if (num == 2) {
+                System.out.println("It is Monday");
+            }
+            if (num == 3) {
+                System.out.println("It is Tuesday");
+            }
+            if (num == 4) {
+                System.out.println("It is Wednesday");
+            }
+            if (num == 5) {
+                System.out.println("It is Thrusday");
+            }
+            if (num == 6) {
+                System.out.println("It is Friday");
+            }
+            if (num == 7) {
+                System.out.println("It is Saturday");
+            }
         }
     }
 }
